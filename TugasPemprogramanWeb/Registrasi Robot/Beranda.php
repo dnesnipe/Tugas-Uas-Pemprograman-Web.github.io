40f65522188f901428662533f29094e1187736f2
Andi Muh Mappanganro <60609883+dnesnipe@users.noreply.github.com>
2021-12-04 23:06:56 +0800

    Add files via upload

---
+++ TugasPemprogramanWeb/Registrasi Robot/Beranda.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>Java Web Media</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link rel="shorcut icon" href="images/jwm-logo-icon.jpg" />
+</head>
+
+<body>
+<div id="container">
+  <div id="header"></div>
+    <!--ini adalah start navigasi-->
+    <div class="nav">
+    	<a href="index.html">Beranda</a>
+        <a href="http://www.javawebmedia.com/news.php">News</a>
+        <a href="http://www.javawebmedia.com/course.php">Course</a>
+      <a href="http://www.javawebmedia.com/contact.php">Contact Us</a>
+  </div>
+    <!-- ini adalah ending navigasi-->
+    <div class="content">
+    
+      <div class="post">
+      <h2><a href="#">Robot TIk</a></h2>
+<p>Kelas Robotik merupakan kelas hasil kerjasama
+    antara Tim Robotron FT-UNM dengan sekolah SMPN 6 Makassar.
+    Kelas robotik ini akan dilaksanakan pada awal semester genap
+    tahun ajaran 2021/2022 yang akan dilaksanakan di SMPN 6 Makassar,
+    Jl. Ahmad Yani No. 25, Baru, Kec. Ujung Pandang, Kota Makassar.</p>
+      </div>
+      
+      <div id="sidebar">
+      <h2>Berita terbaru:</h2>
+      <ul>
+        <li><a href="#">In reprehenderit in voluptate</a></li>
+      </ul>
+<p>&nbsp;</p>
+      
+      </div>
+</body>
+</html>
